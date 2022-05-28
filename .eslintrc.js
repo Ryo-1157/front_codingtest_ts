a87a6f3279a2a8e37fd992f2d96466e415c97610
Ryo-1157 <hit.zjackpot.777@gmail.com>
2022-05-28 21:44:03 +0900

    ESlintとPrettierの設定を完了

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		ecmaVersion: 12,
+		sourceType: 'module',
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['react', '@typescript-eslint', 'unused-imports'],
+	ignorePatterns: ['build'],
+	rules: {
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+		'import/prefer-default-export': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+		],
+		'react/jsx-filename-extension': [
+			'error',
+			{
+				extensions: ['.jsx', '.tsx'],
+			},
+		],
+		'react/react-in-jsx-scope': 'off',
+	},
+};
