0940639c3f354bfed628dfc41ae1e1a14d963e9d
Ryo-1157 <hit.zjackpot.777@gmail.com>
2022-05-29 23:58:12 +0900

    フォルダ名を変更したことに伴い、importの参照先を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from './components/layouts/Header';
+import { Header } from './components/Layouts/Header';
 import { PrefecturePage } from './components/pages/PrefecturePage';
 
 function App() {
