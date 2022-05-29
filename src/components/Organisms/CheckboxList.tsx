64a523af207490f7be198fe3e4cab9b7633b4277
Ryo-1157 <hit.zjackpot.777@gmail.com>
2022-05-30 00:31:37 +0900

    セマンティックコーディングのため、HTMLタグを追加

--- src/components/Organisms/CheckboxList.tsx
+++ src/components/Organisms/CheckboxList.tsx
@@ -5,11 +5,13 @@ export const CheckboxList: React.VFC<PrefectureNameList> = (props) => {
 	const { prefectures, onChange } = props;
 	return (
 		<>
-			{prefectures.map((prefecture) => (
-				<li key={prefecture.prefCode}>
-					<CheckboxItem prefCode={prefecture.prefCode} prefName={prefecture.prefName} onChange={onChange} />
-				</li>
-			))}
+			<ul>
+				{prefectures.map((prefecture) => (
+					<li key={prefecture.prefCode}>
+						<CheckboxItem prefCode={prefecture.prefCode} prefName={prefecture.prefName} onChange={onChange} />
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
